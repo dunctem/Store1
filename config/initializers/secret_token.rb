f936bcf6b590d5555a9273dd578a38e0969d7716
unknown <BOSS@BOSS-PC.(none)>
2012-12-28 15:42:01 -0500

    baseline

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Privee::Application.config.secret_token = '29e7b0b67c2112932f8028ab549d84c16bb4d5e9183510e4fc3ef774c2fb201eeb0f8cc5b0806516e2a37168807d0034c702aff237da8dbbbc9ef320cb46474f'
